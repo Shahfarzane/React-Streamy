605ddaadd99c925909493ffe3f5abf9e3cba39bd
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-07-16 14:16:37 +0200

    Implementing the backend

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';import reducers from './reducers'
 import App from "./components/App";
-
+import reduxThunk from "redux-thunk"
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers,composeEnhancers(applyMiddleware()))
+const store = createStore(reducers,composeEnhancers(applyMiddleware(reduxThunk)))
 ReactDOM.render(
   <Provider store={store}>
     <App />
